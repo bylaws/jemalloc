ac8870dd6bc0573392686680b0e20b050244bf73
Ryan Houdek <Sonicadvance1@gmail.com>
2023-01-09 18:03:31 -0800

    Drop back down to 4k page size. This broke pressure-vessel.

--- pregen/include/jemalloc/internal/jemalloc_internal_defs.h
+++ pregen/include/jemalloc/internal/jemalloc_internal_defs.h
@@ -190,7 +190,7 @@
 /* #undef LG_QUANTUM */
 
 /* One page is 2^LG_PAGE bytes. */
-#define LG_PAGE 14
+#define LG_PAGE 12
 
 /* Maximum number of regions in a slab. */
 /* #undef CONFIG_LG_SLAB_MAXREGS */
